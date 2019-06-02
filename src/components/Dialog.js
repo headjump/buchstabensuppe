fbaccbe28a93d148705a21dfc43be4c22463408c
Dennis Treder <dt@neopoly.de>
2019-06-02 16:47:13 +0200

    On dialog style

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CSSTransition } from 'react-transition-group'
 
 import { closeDialog } from '../state/action'
 
 const Dialog = ({ dialogs, dispatch }) => {
-  if (dialogs.length === 0) { return null }
-
-  const dialog = dialogs[0]
+  const dialog = dialogs[0] || null
+  const visible = !!dialog
 
   return (
-    <div className='dialog__overlay'>
-      <div className='dialog'>
-        {dialog.content}
-        <a onClick={() => { dispatch(closeDialog(dialog.id)) }}>CLOSE</a>
-      </div>
-    </div>
+    <>
+      <CSSTransition in={visible} unmountOnExit timeout={500} classNames="--ani">
+        <div className='dialog__backdrop' />
+      </CSSTransition>
+      <CSSTransition in={visible} unmountOnExit timeout={500} classNames="--ani">
+        <div className='dialog__holder'>
+          <div className='dialog'>
+            {dialog && 
+              <>
+                {dialog.content}
+                <a onClick={() => { visible && dispatch(closeDialog(dialog.id)) }}>CLOSE</a>
+              </>
+            }
+          </div>
+        </div>
+      </CSSTransition>
+    </>
   )
 }
 
